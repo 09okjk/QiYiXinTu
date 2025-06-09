362f89dd0c4d842c180f114974b8cfbe8afd8600
liuhaoyu <1349808913@qq.com>
2025-06-09 13:53:37 +0800

    localization: simplify dialogue end logic and remove unused animation methods

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -113,18 +113,8 @@ public class GameUIManager : MonoBehaviour
         // 处理对话结束后的逻辑
         if (dialogueID == "homework_over")
         {
-            LuXinsheng luXinsheng = NPCManager.Instance.GetNpc("LuXinsheng") as LuXinsheng;
-            if (luXinsheng != null)
-            {
-                luSleepAnimator.gameObject.SetActive(false);
-                luWeekUpAnimator.gameObject.SetActive(true);
-                // luXinsheng.ActivateNpc();
-                // luXinsheng.WeekUp();
-            }
-            else
-            {
-                Debug.LogWarning("LuXinsheng NPC not found.");
-            }
+            luSleepAnimator.gameObject.SetActive(false);
+            luWeekUpAnimator.gameObject.SetActive(true);
         }
 
         if (dialogueID == "fight_dialogue")
@@ -167,20 +157,6 @@ public class GameUIManager : MonoBehaviour
         if (luSleepAnimator != null)
         {
             luSleepAnimator.gameObject.SetActive(true);
-        }
-        else
-        {
-            Debug.LogWarning("Lu Sleep Animator is not assigned.");
-        }
-    }
-    
-    public void StopLuWeekUpAnimation()
-    {
-        if (luWeekUpAnimator != null)
-        {
-            luWeekUpAnimator.StopPlayback();
-            luWeekUpAnimator.gameObject.SetActive(false);
-            // NPCManager.Instance.ShowNpc("LuXinsheng");
         }
         else
         {
